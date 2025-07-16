404c00734f5f878572a8bbbe5587305b87f8ab64
infinitygym <georgiosapostolak@gmail.com>
2025-07-16 14:25:06 +0300

    Update contact-form.tsx

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -45,7 +45,7 @@ export default function ContactForm() {
     formDataToSend.append("_template", "table");
     try {
       const response = await fetch(
-        "https://formsubmit.co/info@kama.cooking",
+        "https://formsubmit.co/info@infinityheraklion@gmail.com",
         {
           method: "POST",
           body: formDataToSend,
